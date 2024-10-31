e5addb7f3f0b7beb8d9c648cc64880cde78e2814
Zach-Hudelson <146473760+Zach-Hudelson@users.noreply.github.com>
2024-10-31 12:32:20 -0400

    Create named-location.js
    The lowest coupled class in our system, and foundation for experience

---
+++ named-location.js
@@ -0,0 +1,34 @@
+// Updates a single a-entity to track a Location with the given name.
+const namedLocationComponent = {
+  schema: {
+    name: {type: 'string'},
+  },
+  init() {
+    const {object3D} = this.el
+    const {name} = this.data
+    this.el.sceneEl.addEventListener('realityready', () => {
+      object3D.visible = false
+    })
+
+    const foundLocation = ({detail}) => {
+      if (name !== detail.name) {
+        return
+      }
+      object3D.position.copy(detail.position)
+      object3D.quaternion.copy(detail.rotation)
+      object3D.visible = true
+    }
+
+    const lostLocation = ({detail}) => {
+      if (name !== detail.name) {
+        return
+      }
+      object3D.visible = false
+    }
+
+    this.el.sceneEl.addEventListener('xrprojectwayspotfound', foundLocation)
+    this.el.sceneEl.addEventListener('xrprojectwayspotlost', lostLocation)
+  },
+}
+
+export {namedLocationComponent}
